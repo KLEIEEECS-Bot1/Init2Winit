4233b271859f577b5bc56233dcfd651bcb981d01
Ishank307 <ishankkumar307@gmail.com>
2025-09-20 15:33:09 +0530

    null

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskRewarderApp } from '@/components/app/task-rewarder-app';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
-      <TaskRewarderApp />
-    </main>
-  );
-}
